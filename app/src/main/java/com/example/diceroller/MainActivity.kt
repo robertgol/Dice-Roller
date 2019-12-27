1a5a43a5cc40a3f69ffb81d5fba1da27545159ed
r.golusinski <r.golusinski@samsung.com>
2019-12-27 09:32:28 +0100

    use dice images, add a tools:src image for the preview of a view in AS

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,22 +2,32 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var diceImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImageView = findViewById(R.id.diceImageView)
         val rollButton = findViewById<Button>(R.id.rollButton)
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
         rollButton.setOnClickListener {
-            rollDice(resultTextView)
+            rollDice()
         }
     }
 
-    private fun rollDice(resultTextView: TextView) {
-        resultTextView.text = Random.nextInt(1, 7).toString()
+    private fun rollDice() {
+        val drawableResource = when (Random.nextInt(1, 7)) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImageView.setImageResource(drawableResource)
     }
 }
