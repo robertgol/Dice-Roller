20afd626b6f64a436657507790e96c3b2b42cb28
r.golusinski <r.golusinski@samsung.com>
2019-12-27 09:16:06 +0100

    implement roll buton on click, add dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        rollButton.setOnClickListener {
+            rollDice(resultTextView)
+        }
+    }
+
+    private fun rollDice(resultTextView: TextView) {
+        resultTextView.text = Random.nextInt(1, 7).toString()
     }
 }
